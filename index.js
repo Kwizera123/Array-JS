0c93e2a6b4468c4f4d84165bc1c20ef4aacecf2c
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-20 18:27:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let arr = ["Apples","Orange","Banana", 75, 59, 105];
+
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[3]);
+console.log(arr[5]);
+//Array is useful, it used to arrenge values in order, make group of values and check value one by one.
+console.log("------------");
+let num = [8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(num[3]);
+console.log(num);
